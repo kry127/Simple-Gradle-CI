c6835ebcab040594c74d0ebe78822cf1de8f635f
Kry127 <kry127@yandex.ru>
2020-02-16 00:51:21 +0300

    Added test output + YAML empty lines dividers

---
+++ src/test/java/ru/spb/kry127/yacli/TestRunnerTest.java
@@ -0,0 +1,13 @@
+package ru.spb.kry127.yacli;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestRunnerTest {
+
+    @Test
+    public void main() {
+        System.out.println("Hello world from test!");
+    }
+}
